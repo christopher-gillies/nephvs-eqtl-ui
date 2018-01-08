504e9f05741f665e0ddceed3d078909f82213b98
christopher-gillies <cgillies@umich.edu>
2018-01-08 16:57:53 -0500

    inserted skeleton code for DAPPlot

--- src/components/ClusterPlot.js
+++ src/components/ClusterPlot.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './BoxPlot.css'
+import './ClusterPlot.css'
 import { select, max, min, scaleLinear, scaleBand, scaleOrdinal, quantile,
 axisLeft, axisBottom, mean, deviation } from 'd3'
 import { schemeAccent } from 'd3-scale-chromatic'
@@ -24,90 +24,9 @@ class BoxPlot extends Component {
    formatDataForD3() {
 
      //perform a shallow copy of the input data
-     const data = this.props.data.slice();
+     const gene = this.props.gene;
+     const clusters = this.probs.clusters;
 
-     //sorting and then grouping is gives the same order as grouping and sorting
-     data.sort( (a,b) => {
-       if(a.y < b.y) {
-         return -1;
-       } else if(a.y > b.y) {
-         return 1;
-       } else {
-         return 0;
-       }
-     })
-
-     const yVals = data.map( a => a.y);
-     const minY = min(yVals)
-     const maxY = max(yVals)
-
-     //Group data by x
-     // x = 1 => [ expr... ]
-     let groupedData = { };
-     for(let i = 0; i < data.length; i++) {
-       let item = data[i];
-       let group = item.group;
-       let y = item.y;
-       if(groupedData[group] !== undefined) {
-         groupedData[group].push(y);
-       } else {
-         groupedData[group] = [ y ]
-       }
-     }
-     //console.log(groupedData)
-
-     let groups = Object.keys(groupedData)
-
-     //if the groupOrder is specified then override the groups variable
-     if(this.props.groupOrder) {
-       groups = this.props.groupOrder;
-     }
-
-     let colorScale = scaleOrdinal(schemeAccent)
-       .domain(groups);
-
-     //format for D3
-     let boxPlotData = [];
-
-     //for each group,
-     //groupVals are the values for this group
-     //so for the genotype of 1, we would have a set of expression values
-     for (let [group, groupVals] of Object.entries(groupedData)) {
-
-       let record = {};
-       let localMin = min(groupVals);
-       let localMax = max(groupVals);
-       let localMean = mean(groupVals)
-       let localStdDev = deviation(groupVals)
-       let quartiles = computeQuartiles(groupVals);
-       let iqr = quartiles[2] - quartiles[0]
-       let lb_iqr = quartiles[0] - 1.5 * iqr;
-       let ub_iqr = quartiles[2] + 1.5 * iqr;
-       let lowerWhisker = max( [localMin, lb_iqr]);
-       let upperWhisker = min( [localMax, ub_iqr]);
-       let outliers = groupVals.filter(function(c) {
-         if(c < lb_iqr || c > ub_iqr) {
-           return true;
-         } else {
-           return false;
-         }
-       });
-       record["group"] = group;
-       record["n"] = groupVals.length;
-       record["vals"] = groupVals;
-       record["outliers"] = outliers.map(function(x) {
-         return { group: group, val: x}
-       });
-       record["quartile"] = quartiles;
-       record["min"] = localMin;
-       record["max"] = localMax;
-       record["mean"] = localMean;
-       record["stdDev"] = localStdDev;
-       record["whiskers"] = [lowerWhisker, upperWhisker];
-       record["color"] = colorScale(group);
-
-       boxPlotData.push(record);
-     }
 
      return {
        "boxPlotData": boxPlotData,
