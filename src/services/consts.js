c6fa8d6c7d4cecce0e9123a9d436d0142cd100dc
christopher-gillies <cgillies@umich.edu>
2018-01-17 16:55:08 -0500

    added auto baseURL configuration

--- src/services/consts.js
+++ src/services/consts.js
@@ -1,2 +1,8 @@
 //we may be able to convert this to be window.location.href
-export const baseUrl = "http://localhost:8080/";
+let port = 8080;
+let re = /^(http(s)?[:][/][/])([^:]+)([:]\d+)?([/].+)?/;
+//export const baseUrl = "http://localhost:8080/";
+let m = re.exec(window.location.href);
+
+export const baseUrl = m[1] + m[3] + ":" + port + "/";
+console.log(baseUrl)
