9335c4be7683e6abc9fee75b4e053d88709b6bcf
christopher-gillies <cgillies@umich.edu>
2018-01-22 12:00:34 -0500

    modified const.js to support reverse proxy and local host

--- src/services/consts.js
+++ src/services/consts.js
@@ -1,8 +1,16 @@
 //we may be able to convert this to be window.location.href
 let port = 8080;
-let re = /^(http(s)?[:][/][/])([^:]+)([:]\d+)?([/].+)?/;
+let re = /^(http(s)?[:][/][/])([^:\/]+)([:]\d+)?([/].+)?/;
 //export const baseUrl = "http://localhost:8080/";
 let m = re.exec(window.location.href);
+let host = m[3];
+let baseUrl = m[1] + m[3] + ":" + port + "/";
 
-export const baseUrl = m[1] + m[3] + ":" + port + "/";
+if(host !== "localhost") {
+  //if this is not running on local host
+  //assume a reverse proxy is running
+  baseUrl = m[1] + m[3] + "/";
+}
+
+export baseUrl;
 console.log(baseUrl)
